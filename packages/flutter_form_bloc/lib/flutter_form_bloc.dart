86575f10b611923c9b518bdb9320d594f5c235ba
Simone <brexmaster@gmai.com>
2020-02-27 20:40:28 +0100

    export switchFieldBlocBuilder

--- packages/flutter_form_bloc/lib/flutter_form_bloc.dart
+++ packages/flutter_form_bloc/lib/flutter_form_bloc.dart
@@ -6,6 +6,7 @@ export 'package:flutter_bloc/flutter_bloc.dart';
 export 'src/field_bloc_builder.dart';
 export 'src/text_field_bloc_builder.dart';
 export 'src/dropdown_field_bloc_builder.dart';
+export 'src/switch_field_bloc_builder.dart';
 export 'src/checkbox_field_bloc_builder.dart';
 export 'src/checkbox_group_field_bloc_builder.dart';
 export 'src/radio_button_group_field_bloc.dart';
