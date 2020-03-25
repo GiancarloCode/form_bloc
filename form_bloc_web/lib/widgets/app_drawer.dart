b02adfddb84c46cee63f27dc353feb541d508dfd
GiancarloCode <GiancarloCode@gmail.com>
2020-03-25 13:12:22 -0500

    fix style

--- form_bloc_web/lib/widgets/app_drawer.dart
+++ form_bloc_web/lib/widgets/app_drawer.dart
@@ -150,7 +150,7 @@ class _AppDrawerState extends State<AppDrawer> with RouteAware {
       alignment: Alignment.centerLeft,
       child: Text(
         'Examples',
-        style: Theme.of(context).textTheme.headline6,
+        style: Theme.of(context).textTheme.title,
       ),
     );
   }
