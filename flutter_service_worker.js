bc59f589b00e46cc312810793c426a6ce11ad624
GiancarloCode <GiancarloCode@users.noreply.github.com>
2020-07-25 06:35:57 +0000

    deploy: e0d16976ecfd80cb2578d40e985704676029767b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "c45571e2d84148a6242f43f0b88a4ee1",
-"assets/NOTICES": "7793559cae0b555f4607f563dc254deb",
+"assets/NOTICES": "1359c1e6f0c843254fd4a0633e2e7372",
 "assets/assets/fonts/JosefinSans-Regular.ttf": "70e2eb768304d11812d28e33e91ecac5",
 "assets/assets/fonts/JosefinSans-Bold.ttf": "0fce6d85ecbbf3d97e0d848824454600",
 "assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
@@ -17,7 +17,7 @@ const RESOURCES = {
 "assets/lib/examples/wizard_form.dart": "b9925dbac8d53cd1892c0fe01065c628",
 "assets/lib/examples/serialized_form.dart": "54ebeea60c5a13fc13724ac1fa6d365c",
 "assets/lib/examples/loading_and_initializing_form.dart": "6f8b653a0d5e3a175cacfcd9ff5d0d54",
-"assets/lib/examples/list_fields_form.dart": "6204a9e473c5f449a41629e643af2388",
+"assets/lib/examples/list_fields_form.dart": "08c899fd5b231de1bbd096b1a6837127",
 "assets/lib/examples/validation_based_on_other_field.dart": "add7a6b3e888b964c089bb012d9ef3e5",
 "assets/lib/examples/simple_form.dart": "130d969c08b00e847b1db0f8826b5097",
 "assets/lib/examples/submission_progress_form.dart": "ff0c7884fc01e7ccf403792885a1244c",
@@ -28,7 +28,7 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "83fc3c8c3037f770d8b55bf058ece138",
 "/": "83fc3c8c3037f770d8b55bf058ece138",
-"main.dart.js": "5d6db9d12b46acdc7fbf1d72113c1bd2"
+"main.dart.js": "9a7ed9194455114625368b48eb0cdc04"
 };
 
 // The application shell files that are downloaded before a service worker can
