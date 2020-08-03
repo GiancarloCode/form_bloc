3660e51ffd75a1dee30eef8900738434637b6db8
GiancarloCode <GiancarloCode@gmail.com>
2020-08-03 17:37:09 -0500

    fix pedantic issues

--- packages/form_bloc/lib/src/blocs/field/field_bloc.dart
+++ packages/form_bloc/lib/src/blocs/field/field_bloc.dart
@@ -80,13 +80,13 @@ abstract class SingleFieldBloc<
 
   final Duration _asyncValidatorDebounceTime;
 
+  /* Previously used to simplify initial state creation
+
   final Suggestions<Suggestion> _suggestions;
-
   final String _name;
-
   final dynamic Function(Value value) _toJson;
-
   final ExtraData _extraData;
+  */
 
   final PublishSubject<Value> _asyncValidatorsSubject = PublishSubject();
   StreamSubscription<UpdateFieldBlocStateError> _asyncValidatorsSubscription;
@@ -100,15 +100,14 @@ abstract class SingleFieldBloc<
     List<Validator<Value>> validators,
     List<AsyncValidator<Value>> asyncValidators,
     this._asyncValidatorDebounceTime,
-    this._suggestions,
+    Suggestions<Suggestion> suggestions,
     String name,
-    this._toJson,
-    this._extraData,
+    dynamic Function(Value value) toJson,
+    ExtraData extraData,
     State initialState,
   )   : assert(_asyncValidatorDebounceTime != null),
         _validators = validators ?? [],
         _asyncValidators = asyncValidators ?? [],
-        _name = name ?? Uuid().v1(),
         super(initialState) {
     FormBlocObserver.overrideDelegateOfBlocSupervisor();
     _setUpAsyncValidatorsSubscription();
