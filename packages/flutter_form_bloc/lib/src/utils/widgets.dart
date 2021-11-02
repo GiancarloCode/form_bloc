e01a1dc9d524a329174336fc83be0a6186ee514f
Simone Bressan <simone@kuama.net>
2021-11-02 21:42:44 +0100

    Merged and removed duplicate and/or copied dropdown code Now the dropdown will not open when it acquires focus Reduced and simplified the dropdown code

--- packages/flutter_form_bloc/lib/src/utils/widgets.dart
+++ packages/flutter_form_bloc/lib/src/utils/widgets.dart
@@ -3,28 +3,31 @@ import 'package:flutter_form_bloc/src/utils/utils.dart';
 
 class DefaultFieldBlocBuilderTextStyle extends StatelessWidget {
   final bool isEnabled;
+  final TextStyle? style;
   final Widget child;
 
   const DefaultFieldBlocBuilderTextStyle({
     Key? key,
     required this.isEnabled,
+    this.style,
     required this.child,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
-      style: Style.getDefaultTextStyle(
-        context: context,
-        isEnabled: isEnabled,
-      )!,
+      style: style ??
+          Style.getDefaultTextStyle(
+            context: context,
+            isEnabled: isEnabled,
+          )!,
       child: child,
     );
   }
 }
 
 class DefaultFieldBlocBuilderPadding extends StatelessWidget {
-  final EdgeInsets? padding;
+  final EdgeInsetsGeometry? padding;
   final Widget child;
 
   const DefaultFieldBlocBuilderPadding({
