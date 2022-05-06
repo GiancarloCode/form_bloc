c696fb1bc22c4494a0ca8bd8aae6da61de9f3faa
Eric hui <eric.hui@thegulu.com>
2022-05-06 11:34:52 +0800

    add test for list_field_bloc_test.dart

--- packages/form_bloc/test/list_field/list_field_bloc_test.dart
+++ packages/form_bloc/test/list_field/list_field_bloc_test.dart
@@ -82,5 +82,160 @@ void main() {
         list.addFieldBloc(boolean1);
       });
     });
+
+    group('clear', () {
+      test('clear', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2],
+        );
+
+        final expected = createListState<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [],
+        );
+
+        expect(
+          list.stream,
+          emitsInOrder(<ListFieldBlocState>[expected]),
+        );
+
+        list.clearFieldBlocs();
+      });
+    });
+
+    group('Insert', () {
+      test('Insert item', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean2],
+        );
+
+        final expected = createListState<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2],
+        );
+
+        expect(
+          list.stream,
+          emitsInOrder(<ListFieldBlocState>[expected]),
+        );
+
+        list.insertFieldBloc(boolean1, 0);
+      });
+
+      test('Insert list', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+        final boolean3 = BooleanFieldBloc<Object>(name: '3');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean3],
+        );
+
+        final expected = createListState<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2, boolean3],
+        );
+
+        expect(
+          list.stream,
+          emitsInOrder(<ListFieldBlocState>[expected]),
+        );
+
+        list.insertFieldBlocs([boolean1, boolean2], 0);
+      });
+
+      test('Insert empty items', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2],
+        );
+
+        final previousState = list.state;
+
+        list.insertFieldBlocs([], 0);
+
+        expect(previousState, list.state);
+      });
+    });
+
+    group('Update', () {
+      test('Reorder', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2],
+        );
+
+        final expected = createListState<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean2, boolean1],
+        );
+
+        expect(
+          list.stream,
+          emitsInOrder(<ListFieldBlocState>[expected]),
+        );
+
+        list.updateFieldBlocs([boolean2, boolean1]);
+      });
+
+      test('Update to empty', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2],
+        );
+
+        final expected = createListState<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [],
+        );
+
+        expect(
+          list.stream,
+          emitsInOrder(<ListFieldBlocState>[expected]),
+        );
+
+        list.updateFieldBlocs([]);
+      });
+
+      test('Update from empty', () {
+        final boolean1 = BooleanFieldBloc<Object>(name: '1');
+        final boolean2 = BooleanFieldBloc<Object>(name: '2');
+
+        final list = ListFieldBloc<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [],
+        );
+
+        final expected = createListState<BooleanFieldBloc<Object>, dynamic>(
+          name: 'list',
+          fieldBlocs: [boolean1, boolean2],
+        );
+
+        expect(
+          list.stream,
+          emitsInOrder(<ListFieldBlocState>[expected]),
+        );
+
+        list.updateFieldBlocs([boolean1, boolean2]);
+      });
+    });
   });
 }
