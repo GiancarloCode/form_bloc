354c9125994c1cc05943965d6b733f46c99199df
538023 <83300538+538023@users.noreply.github.com>
2022-07-09 15:24:16 +0200

    add FieldBlocUtils

--- packages/form_bloc/lib/form_bloc.dart
+++ packages/form_bloc/lib/form_bloc.dart
@@ -1,7 +1,8 @@
 library form_bloc;
 
 export 'src/blocs/form/form_bloc.dart';
-export 'src/blocs/field/field_bloc.dart' hide FormBlocUtils;
+export 'src/blocs/field/field_bloc.dart';
+export 'src/blocs/field/field_bloc_utils.dart';
 export 'src/blocs/form_bloc_observer.dart';
 export 'src/validators/field_bloc_validators.dart';
 export 'src/utils.dart';
