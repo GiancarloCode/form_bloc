0c8bd29577d5fdffa37a4263ffd909b39827dca7
538023 <83300538+538023@users.noreply.github.com>
2022-07-09 14:57:29 +0200

    export Param

--- packages/form_bloc/lib/form_bloc.dart
+++ packages/form_bloc/lib/form_bloc.dart
@@ -4,3 +4,4 @@ export 'src/blocs/form/form_bloc.dart';
 export 'src/blocs/field/field_bloc.dart' hide FormBlocUtils;
 export 'src/blocs/form_bloc_observer.dart';
 export 'src/validators/field_bloc_validators.dart';
+export 'src/utils.dart';
